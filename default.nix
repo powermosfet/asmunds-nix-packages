d18247a51e44d076aa23e7200d653188573bedd2
Asmund Berge <herrasbe@gmail.com>
2021-03-21 12:20:27 +0100

    Define callPackage

--- default.nix
+++ default.nix
@@ -1,5 +1,9 @@
-{ }:
-
+{ pkgs: import <nixpkgs> { } }:
+let
+  callPackage = newScope {
+    overrides = pkgs.haskell.packageOverrides;
+  };
+in
 {
   hledger-match = callPackage ./hledger-match.nix { };
 }
