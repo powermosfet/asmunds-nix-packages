85fbbb839be17bf43b20e3cb7c3f8ae854b1ac67
Asmund Berge <herrasbe@gmail.com>
2021-03-30 20:56:48 +0200

    Update xtensa toolchain revision

--- xtensa-esp8266-toolchain.nix
+++ xtensa-esp8266-toolchain.nix
@@ -3,7 +3,7 @@ let
   src = pkgs.fetchFromGitHub {
     owner = "powermosfet";
     repo  = "nix-xtensa-esp8266-toolchain";
-    rev = "592a305dba108adf38f0d48c2e2b10e14acc83fa";
+    rev = "9c6f78cc1ec02b2ad2e5022ee380e9d14461046a";
     sha256 = "0r8vnfh7pk2flq1mrkfnc73zjmhx1bx9082gy3zcxxc38r4794xc";
   };
 
