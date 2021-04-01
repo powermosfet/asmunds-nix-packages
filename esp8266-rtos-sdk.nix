36def4d4ff55f7d1c73951e77e56a864a0309034
Asmund Berge <herrasbe@gmail.com>
2021-04-01 10:36:52 +0200

    Match versions

--- esp8266-rtos-sdk.nix
+++ esp8266-rtos-sdk.nix
@@ -1,17 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  toolchain = import ./xtensa-esp8266-toolchain.nix { };
-in
-
 pkgs.stdenv.mkDerivation {
   version = "v3.4.0";
   name = "esp8266-rtos-sdk";
   src = pkgs.fetchFromGitHub {
     owner = "espressif";
     repo  = "ESP8266_RTOS_SDK";
-    rev = "89a3f254b63819035f65d9c5dcdae8864f1a6a8a";
-    sha256 = "1a47n8nch0xr9103n91vqbph54ap12p5xij2iv2m2xpxds4qiryz";
+    rev = "4ce354c4686ee75c54abb57c5031f9226436fa7b";
+    sha256 = "1m2lnjs1bdjq3p88wa7x7w5lq73w4ndza3d2z5sk3qr8ld1m4m6g";
   };
 
   dontConfigure = true;
--- xtensa-esp8266-toolchain.nix
+++ xtensa-esp8266-toolchain.nix
@@ -1,11 +1,11 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.stdenv.mkDerivation {
-  version = "v8.4.0";
+  version = "v5.2.0";
   name = "xtensa-esp8266-toolchain";
   src = builtins.fetchTarball {
-    url = "https://dl.espressif.com/dl/xtensa-lx106-elf-gcc8_4_0-esp-2020r3-linux-amd64.tar.gz";
-    sha256 = "1w11nl02hp2sg0pmykq67c4s9nlbbkikf6x2zs1lvp5i2jk3knpa";
+    url = "https://dl.espressif.com/dl/xtensa-lx106-elf-linux64-1.22.0-100-ge567ec7-5.2.0.tar.gz";
+    sha256 = "0nga2x064jaslqq35wxi2ssrh9rgs3f3gj5pkri0d2p7nmd8ilk7";
   };
 
   dontConfigure = true;
