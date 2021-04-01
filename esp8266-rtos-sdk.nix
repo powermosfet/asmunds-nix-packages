8e511a3232c13d0807821b1d9b0d7fb2346df3d8
Asmund Berge <herrasbe@gmail.com>
2021-04-01 10:55:51 +0200

    Fetch submodules equals true

--- esp8266-rtos-sdk.nix
+++ esp8266-rtos-sdk.nix
@@ -8,6 +8,7 @@ pkgs.stdenv.mkDerivation {
     repo  = "ESP8266_RTOS_SDK";
     rev = "4ce354c4686ee75c54abb57c5031f9226436fa7b";
     sha256 = "1m2lnjs1bdjq3p88wa7x7w5lq73w4ndza3d2z5sk3qr8ld1m4m6g";
+    fetchSubmodules = true;
   };
 
   dontConfigure = true;
