b4bb3c35e813130549ef81a8c5f9487e46572b9c
Asmund Berge <herrasbe@gmail.com>
2021-04-01 10:58:49 +0200

    Also update hash

--- esp8266-rtos-sdk.nix
+++ esp8266-rtos-sdk.nix
@@ -7,7 +7,7 @@ pkgs.stdenv.mkDerivation {
     owner = "espressif";
     repo  = "ESP8266_RTOS_SDK";
     rev = "4ce354c4686ee75c54abb57c5031f9226436fa7b";
-    sha256 = "1m2lnjs1bdjq3p88wa7x7w5lq73w4ndza3d2z5sk3qr8ld1m4m6g";
+    sha256 = "13bjjb1j9yqsrdl4gkcdcfranly1nxk4ccfd91bbgfkyv4693asl";
     fetchSubmodules = true;
   };
 
