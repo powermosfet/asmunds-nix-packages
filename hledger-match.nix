e471aee35b2d8fe09975652b5932c1f00e07463d
Asmund Berge <herrasbe@gmail.com>
2021-04-25 17:55:10 +0200

    Update hledger-match

--- hledger-match.nix
+++ hledger-match.nix
@@ -4,7 +4,7 @@ let
     owner = "powermosfet";
     repo  = "hledger-match";
     rev = "70e00e918087bbb30c3d253923a22c9196a4b03b";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    sha256 = "0qyqrz7gpvm0p2smh739j11l2dq4ii7dk0ny3qs2633p79ijar6m";
   };
 
 in
